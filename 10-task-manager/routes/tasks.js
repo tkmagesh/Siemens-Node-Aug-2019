c12512229f8c017fbfd3e722bd60809ecf7deec3
Magesh Kuppan <tkmagesh77@gmail.com>
2019-08-08 13:50:32 +0530

    using async await

--- 10-task-manager/routes/tasks.js
+++ 10-task-manager/routes/tasks.js
@@ -2,31 +2,35 @@ const express = require('express'),
 	router = express.Router(),
 	taskService = require('../services/taskService');
 
+/*
 router.get('/', function(req, res, next){
 	taskService.getAll()
 		.then(function(taskList){
 			res.json(taskList);	
 		});
+});*/
+
+//using async await
+
+router.get('/', async function(req, res, next){
+	const taskList = await taskService.getAll()
+	res.json(taskList);	
 });
 
-router.get('/:id', function(req, res, next){
+router.get('/:id', async function(req, res, next){
 	const taskId = parseInt(req.params.id);
-	taskService.get(taskId)
-		.then(function(result){
-			if (result){
-				res.json(result); 
-			} else {
-				res.status(404).end();
-			}
-		});
+	const result = await taskService.get(taskId)
+	if (result){
+		res.json(result); 
+	} else {
+		res.status(404).end();
+	}
 });
 
-router.post('/', function(req, res, next){
+router.post('/', async function(req, res, next){
 	const taskData = req.body;
-	taskService.addNew(taskData)
-		.then(function(newTask){
-			res.status(201).json(newTask);	
-		});
+	const newTask = await taskService.addNew(taskData);
+	res.status(201).json(newTask);	
 });
 
 module.exports = router;
--- 10-task-manager/services/taskService.js
+++ 10-task-manager/services/taskService.js
@@ -4,28 +4,19 @@ function getAll(callback){
 	return taskDb.readData();	
 }
 
-function get(id, callback){
-	return taskDb
-		.readData()
-		.then(function(taskList){
-			const result = taskList.find(task => task.id === taskId);
-			return result;
-		});
+async function get(id, callback){
+	const taskList = await taskDb.readData();
+	const result = taskList.find(task => task.id === taskId);
+	return result;
 }
 
-function addNew(taskData, callback){
-	return taskDb
-		.readData()
-		.then(function(taskList){
-			const newTaskId = taskList.reduce((result, task) => result > task.id ? result : task.id, 0) + 1;
-			const newTask = { ...taskData, id : newTaskId}
-			taskList.push(newTask);
-			return taskDb
-				.saveData(taskList)
-				.then(function(){
-					return newTask;
-				});
-		});
+async function addNew(taskData, callback){
+	const taskList = await taskDb.readData()
+	const newTaskId = taskList.reduce((result, task) => result > task.id ? result : task.id, 0) + 1;
+	const newTask = { ...taskData, id : newTaskId}
+	taskList.push(newTask);
+	await taskDb.saveData(taskList)
+	return newTask;
 }
 
 const taskService = { 
